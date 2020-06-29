8ba36dd394a49b9f32158a11b0713dff52c7b0a5
Donna Torres <todonnatorres@icloud.com>
2020-06-29 00:39:34 -0500

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,34 @@
 class Pokemon
+    attr_accessor :id, :name, :type, :db
+
+    def initialize(id:, name:, type:, db:)
+        @id = id
+        @name = name
+        @type = type
+        @db = db                
+    end
+
+    def self.save(name, type, db)
+        db.execute("INSERT INTO pokemon (name, type) VALUES (?,?);", name, type)
+    end
+
+    def self.find(id, db)
+        sql =<<-SQL
+        SELECT * FROM pokemon
+        WHERE id = ?
+        
+        SQL
+    
+        data = db.execute(sql, id).flatten
+        id, name, type = data
+        Pokemon.new(id: id, name: name, type: type, db: db)
+    end
 end
+
+
+
+
+
+
+# initialize requires keyword argumetns. 
+# dont insert string interpolation into query. Sanitize(filter) to remove all dangerous characters from an input string before passing it to the SQL engine or parameterize aka parsed (prepared) statement = pre compiling SQL statements. so that you dont lose info
